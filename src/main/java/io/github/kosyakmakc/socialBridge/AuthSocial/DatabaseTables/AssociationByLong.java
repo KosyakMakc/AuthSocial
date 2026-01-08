e741d3cea0220a204dea1d7041561a13862bf8a0
Човнюк Максим <kosyakmakc@live.com>
2026-01-08 16:00:58 +0500

    dev fix table definition (#9)

--- src/main/java/io/github/kosyakmakc/socialBridge/AuthSocial/DatabaseTables/AssociationByLong.java
+++ src/main/java/io/github/kosyakmakc/socialBridge/AuthSocial/DatabaseTables/AssociationByLong.java
@@ -2,17 +2,27 @@ package io.github.kosyakmakc.socialBridge.AuthSocial.DatabaseTables;
 
 import java.util.UUID;
 
+import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
 @DatabaseTable(tableName = AssociationByLong.TABLE_NAME)
-public class AssociationByLong extends Association<Long> {
+public class AssociationByLong extends Association {
     public static final String TABLE_NAME = "authsocial_association_long";
+
+    @DatabaseField(columnName = SOCIAL_USER_ID_FIELD_NAME, index = true)
+    private Long socialUserId;
 
     public AssociationByLong() {
 
     }
 
-    public AssociationByLong(UUID minecraftId, UUID socialPlatformId, Long socialuserId) {
-        super(minecraftId, socialPlatformId, socialuserId);
+    public AssociationByLong(UUID minecraftId, UUID socialPlatformId, Long socialUserId) {
+        super(minecraftId, socialPlatformId);
+        this.socialUserId = socialUserId;
+    }
+
+    @Override
+    public Long getSocialUserId() {
+        return socialUserId;
     }
 }
